f4ce761cd5727f188fed8d42cdeb6c7f34ec344e
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-03 17:12:29 -0300

    criado teste2.py

---
+++ teste.py/teste2.py
@@ -0,0 +1,17 @@
+# executando código do desafio de conceitos básicos de python
+
+def pular_nuvens(nuvens):
+    saltos = 0
+    indice = 0
+    while indice < (len(nuvens) - 2):
+        if nuvens [indice + 2] != 1:
+            saltos = 1
+            indice = 2
+        else:
+            saltos = 1 
+            indice = 1
+    return saltos 
+
+print (pular_nuvens(nuvens))
+
+#código imcompleto impossibilitando conclusão do teste
