fd8e88bd1c18c25b2f215ffd0d55057c5c7174ae
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-10 12:34:02 -0300

    criado exercicio1.py

---
+++ exercicios-aula/exercicio1.py
@@ -0,0 +1,25 @@
+#exercicio da aula do dia 09/03/2026
+
+carroPasseio = 10.00
+caminhaoEixo = 15.00
+motocicleta = 5.00
+veiculoOficial = 0
+
+valoresBase = [10.00, 15.00, 5.00, 0]
+horarioPassagem = [00.00 - 23.59]
+
+def calculo_taxa():
+    global carroPasseio, caminhaoEixo, motocicleta, veiculoOficial
+    taxa = 
+
+    
+
+
+def horario_pico():
+    if horarioPassagem < 06.00 > 09.00 < 17.00 > 20.00:
+        valoresBase = calculo_taxa()
+
+    
+print()
+
+        
