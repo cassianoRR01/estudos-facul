549bcada1fbe1e90f80ffd23dcf9abe8106009ad
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-12 15:25:01 -0300

    Create testeNp2.py

---
+++ teste.py/testeNp2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+soma = num1 + num2
+
+print("Primeiro numero: ",num1)
+print("Segundo numero:", num2)
+print("A soma é: ", soma)
