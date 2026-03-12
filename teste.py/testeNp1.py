bf474f9598ecc6bcf8fcc616970d13237264605f
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-12 14:40:00 -0300

    Create testeNp1.py

---
+++ teste.py/testeNp1.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+soma = num1 + num2
+subt = num1 - num2
+divi = num1 / num2
+multi = num1 * num2
+
+print("Primeiro numero: ", num1)
+print("Segundo numero: ", num2)
+print(f'Resultado: soma = {soma}/ subtração = {subt}/ divisão = {divi}/ multi = {multi}.')
