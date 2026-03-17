1b4aeba5813c30b41e2a534368cd3a1722e3ac68
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-17 14:28:11 -0300

    desafio3 e teste9 add

--- exercícios-AVA/desafio03.py
+++ exercícios-AVA/desafio03.py
@@ -1,3 +1,28 @@
-#dados de entrada
-nomeAluno = input("Nome do aluno: ")
-notas = 
+nomeAluno = input("Digite o nome do aluno: ")
+acumulador = 0.0
+somaNotas = 0
+
+while True:
+    print("Nome do aluno: ", nomeAluno)
+    nota = input("Digite uma nota(ou fim para encerrar): ")
+    if nota.lower() == "fim":
+        break 
+    
+    nota = float(nota)
+    somaNotas = somaNotas + nota
+    acumulador = acumulador + 1
+
+if acumulador > 0:
+    media = somaNotas / acumulador
+else:
+    media = 0
+
+
+
+print("Nome do aluno: ", nomeAluno)
+print("Media: ", media)
+
+
+
+
+
