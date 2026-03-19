cf392caae0c576fd7fa2c3c07720f69def1c6eb9
Cassiano de Medeiros <sircassiano5@gmail.com>
2026-03-19 19:10:07 -0300

    Update exercicio4.py

--- exercicios-aula/exercicio4.py
+++ exercicios-aula/exercicio4.py
@@ -1,2 +1,24 @@
-dinheiro = float(input("Insira o valor: "))
+opc = int(input("Qual conversão fazer (1 - dolar, 2 - euro, 3 - libra): "))
+
+if opc == 1:
+   valor = float(input("Insira o valor: "))
+   valor = valor * 5.22
+   print(f"Valor: {valor:.2f}")
+elif opc == 2:
+   valor = float(input("Insira o valor: "))
+   valor = valor * 6.05
+   print(f"Valor: {valor:.2f}")
+elif opc == 3:
+   valor = float(input("Insira o valor: "))
+   valor = valor * 7.02
+   print(f"Valor: {valor:.2f}")
+else:
+   print("Opção invválida.")
+
+
+
+   
+
+
+
 
